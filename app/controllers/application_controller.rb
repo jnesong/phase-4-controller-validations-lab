4f4e534e5e133ae64f7da031f8343c3187d07a85
Jenny Chau <jnechau@gmail.com>
2022-01-11 22:32:53 -0500

    commit tree 6c2e715d7e11b5338dd3d3fba16dadcd709ccfa7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+# rescue_from ActiveRecord::RecordInvalid, with: :render_unprocess_response
+
+# private
+
+# def render_unprocess_response(invalid)
+#     render json: {errors: invalid.record.errors.full_messages}, status: unprocessable_entity 
+# end
+
 end
